085e427efd3aeefeb1b10993a7d5bd46934ccf14
Architagarg0508 <gargarchita@gmail.com>
2025-09-12 12:47:40 +0530

    Grid and list view

--- src/app/components/Feature.js
+++ src/app/components/Feature.js
@@ -1,11 +1,12 @@
 import Image from "next/image";
-import styles from './Feature.module.css';
+import styles from "./Feature.module.css";
 
-export default function Feature() {
+export default function Feature({ viewmode = "list" }) {
+  console.log(viewmode);
   return (
     <section className={styles.topSection}>
       <picture className={styles.left}>
-<Image
+        <Image
           src="/starfield.svg"
           alt="Starfield background representing AI tech"
           width={586}
@@ -13,21 +14,27 @@ export default function Feature() {
           className={styles.stickyImage}
         />
       </picture>
-     
+
       <div className={styles.right}>
         <header>
           <h4 className={styles.title}>
             A World <br /> Supercomputer
           </h4>
+
           <p className={styles.tag}>/ THE NEXUS LAYER 1</p>
         </header>
         <picture className={styles.pluscircle}>
-          <Image src="/CirclePlus.svg" width={16}
-          height={16} alt="circle plus"/>
+          <Image
+            src="/CirclePlus.svg"
+            width={16}
+            height={16}
+            alt="circle plus"
+          />
         </picture>
         <p className={styles.desc}>
-          Nexus is a new kind of Layer 1 blockchain — purpose-built for the AI era.
-          It’s verifiable. Exponentially scalable. Built for agents. And open to anyone.
+          Nexus is a new kind of Layer 1 blockchain — purpose-built for the AI
+          era. It’s verifiable. Exponentially scalable. Built for agents. And
+          open to anyone.
         </p>
 
         <div className={styles.buttonGroup} aria-label="Main actions">
@@ -37,38 +44,66 @@ export default function Feature() {
               <span className={styles.arrow}>
                 <img src="/Arrows.svg"></img>
               </span>
-              
+              <span className={styles.arrow2}>
+                <img src="/Arrows.svg"></img>
+              </span>
             </span>
-            
           </button>
           <button className={`${styles.btn} ${styles.outline}`}>
-            Button Name<span className={styles.arrow}><img src="/Arrows.svg"></img></span>
+            Button Name
+            <span className={styles.arrow}>
+              <img src="/Arrows.svg"></img>
+            </span>
           </button>
         </div>
-        
+        {viewmode === "list" && (
           <ul className={styles.features} aria-label="Key features">
-            {Array(3)
+            {Array(4)
               .fill(0)
               .map((_, i) => (
                 <li className={styles.feature} key={i}>
-                <div className={styles.featureLeft}>
+                  <div className={styles.featureLeft}>
                     <h3>Horizontally + Vertically Scalable</h3>
-                </div>
-                <div className={styles.featureRight}>
-                <p>
-                  The Nexus Layer 1 is EVM-compatible and optimized for verifiable compute.
-                  Use Rust or Solidity to deploy applications that invoke zk-proven computation
-                  or power agentic services.
-                </p>
-                <figure className={styles.icon}>
-                  <img src="/Socials.svg" alt="Icon" />
-                </figure>
-                    </div>
+                  </div>
+                  <div className={styles.featureRight}>
+                    <p>
+                      The Nexus Layer 1 is EVM-compatible and optimized for
+                      verifiable compute. Use Rust or Solidity to deploy
+                      applications that invoke zk-proven computation or power
+                      agentic services.
+                    </p>
+                    <figure className={styles.icon}>
+                      <img src="/Socials.svg" alt="Icon" />
+                    </figure>
+                  </div>
                 </li>
-
               ))}
           </ul>
-       
+        )}
+        {viewmode === "grid" && (
+          <ul className={styles.featuresgrid} aria-label="Key features">
+            {Array(4)
+              .fill(0)
+              .map((_, i) => (
+                <li className={styles.featuregrid} key={i}>
+                  <figure className={styles.icon}>
+                    <img src="/Socials.svg" alt="Icon" />
+                  </figure>
+                  <div className={styles.featuregridtop}>
+                    <h3>Horizontally + Vertically Scalable</h3>
+                  </div>
+                  <div className={styles.featuregridRight}>
+                    <p>
+                      The Nexus Layer 1 is EVM-compatible and optimized for
+                      verifiable compute. Use Rust or Solidity to deploy
+                      applications that invoke zk-proven computation or power
+                      agentic services.
+                    </p>
+                  </div>
+                </li>
+              ))}
+          </ul>
+        )}
       </div>
     </section>
   );
